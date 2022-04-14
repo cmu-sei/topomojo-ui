04e53ca84cafdcf5d96ad2e68f4488a555a6db86
Justin Britton <jbritton@sei.cmu.edu>
2022-04-14 08:25:49 -0400

    Add confirmation to End Session

---
+++ projects/topomojo-launchpoint/src/app/utility/confirm-button/confirm-button.component.ts
@@ -0,0 +1,28 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { faCheck, faTimes } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-confirm-button',
+  templateUrl: './confirm-button.component.html',
+  styleUrls: ['./confirm-button.component.scss']
+})
+export class ConfirmButtonComponent implements OnInit {
+  @Input() btnClass = 'btn';
+  @Input() disabled = false;
+  @Output() confirm = new EventEmitter<boolean>();
+  confirming = false;
+
+  faCheck = faCheck;
+  faTimes = faTimes;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  continue(yes?: boolean): void {
+    if (!!yes) { this.confirm.emit(true); }
+    this.confirming = false;
+  }
+
+}
