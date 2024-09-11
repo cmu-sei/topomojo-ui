6e15832863c13105a86a1ed738d6685b4d10a419
Tim Spencer <72101647+sei-tspencer@users.noreply.github.com>
2024-09-11 10:33:19 -0400

    Feature/workspace upload download (#17)
    allow zip file upload and download of workspaces

--- projects/topomojo-work/src/app/admin/workspace-browser/workspace-browser.component.ts
+++ projects/topomojo-work/src/app/admin/workspace-browser/workspace-browser.component.ts
@@ -1,8 +1,19 @@
 // Copyright 2021 Carnegie Mellon University.
 // Released under a 3 Clause BSD-style license. See LICENSE.md in the project root.
 
-import { Component, OnInit } from '@angular/core';
-import { faCheckSquare, faSquare, faTrash, faCheck, faSyncAlt, faList, faSearch, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+  faUpload,
+  faDownload,
+  faTrash,
+  faList,
+  faSearch,
+  faPlus,
+  faMinus,
+  faTimes,
+  faFile,
+  faFolder,
+} from '@fortawesome/free-solid-svg-icons';
 import { BehaviorSubject, interval, merge, Observable } from 'rxjs';
 import { debounceTime, filter, switchMap, tap } from 'rxjs/operators';
 import { Search, Workspace, WorkspaceSummary } from '../../api/gen/models';
@@ -11,49 +22,57 @@ import { WorkspaceService } from '../../api/workspace.service';
 @Component({
   selector: 'app-workspace-browser',
   templateUrl: './workspace-browser.component.html',
-  styleUrls: ['./workspace-browser.component.scss']
+  styleUrls: ['./workspace-browser.component.scss'],
 })
 export class WorkspaceBrowserComponent implements OnInit {
   refresh$ = new BehaviorSubject<boolean>(true);
   source$: Observable<WorkspaceSummary[]>;
   source: WorkspaceSummary[] = [];
+  selected$ = new BehaviorSubject<WorkspaceSummary[]>([]);
   selected: WorkspaceSummary[] = [];
   viewed: WorkspaceSummary | undefined = undefined;
   viewChange$ = new BehaviorSubject<WorkspaceSummary | undefined>(this.viewed);
   detail$: Observable<Workspace>;
-  search: Search = { term: '', skip: 0, take: 100};
+  search: Search = { term: '', skip: 0, take: 100 };
   skip = 0;
   take = 100;
   count = 0;
   showCreatePanel = false;
   faPlus = faPlus;
+  faMinus = faMinus;
   faTrash = faTrash;
   faList = faList;
   faSearch = faSearch;
+  faUpload = faUpload;
+  faDownload = faDownload;
+  faTimes = faTimes;
+  faFile = faFile;
+  faFolder = faFolder;
+  readyToDownload = false;
+  isDownloading = false;
+  showUpload = false;
+  showDownload = false;
+  selectDownloads = false;
+  @ViewChild('jsonInput') jsonInput!: ElementRef<HTMLInputElement>;
+  @ViewChild('zipInput') zipInput!: ElementRef<HTMLInputElement>;
 
-  constructor(
-    private api: WorkspaceService,
-  ) {
-    this.source$ = merge(
-      this.refresh$,
-      interval(60000)
-    ).pipe(
+  constructor(private api: WorkspaceService) {
+    this.source$ = merge(this.refresh$, interval(60000)).pipe(
       debounceTime(500),
       switchMap(() => this.api.list(this.search)),
       // tap(r => r.forEach(g => g.checked = !!this.selected.find(s => s.id === g.id))),
-      tap(r => this.source = r),
-      tap(r => this.count = r.length),
-      tap(() => this.review()),
+      tap((r) => (this.source = r)),
+      tap((r) => (this.count = r.length)),
+      tap(() => this.review())
     );
 
     this.detail$ = this.viewChange$.pipe(
-      filter(g => !!g),
-      switchMap(g => api.load(g?.id || ''))
+      filter((g) => !!g),
+      switchMap((g) => api.load(g?.id || ''))
     );
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   refresh(): void {
     this.search.skip = this.skip;
@@ -77,20 +96,16 @@ export class WorkspaceBrowserComponent implements OnInit {
   }
 
   review(): void {
-    this.viewed = this.source.find(g => g.id === this.viewed?.id);
+    this.viewed = this.source.find((g) => g.id === this.viewed?.id);
   }
 
   delete(w: WorkspaceSummary): void {
     this.api.delete(w.id).subscribe(() => {
-      const found = this.source.find(f => f.id === w.id);
+      const found = this.source.find((f) => f.id === w.id);
       if (found) {
-        this.source.splice(
-          this.source.indexOf(found),
-          1
-        );
+        this.source.splice(this.source.indexOf(found), 1);
       }
     });
-
   }
 
   update(w: Workspace): void {
@@ -99,5 +114,155 @@ export class WorkspaceBrowserComponent implements OnInit {
 
   trackById(index: number, g: WorkspaceSummary): string {
     return g.id;
+  }
+
+  select(event: any, workspace: WorkspaceSummary): void {
+    if (event.target.checked) {
+      this.selected.push(workspace);
+      this.selected.sort((a, b) =>
+        a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1
+      );
+    } else {
+      const index = this.selected.findIndex((w) => w.id === workspace.id);
+      if (index > -1) {
+        this.selected.splice(index, 1);
+      }
+    }
+    // have to refresh the BehaviorSubject to refresh the display
+    if (this.readyToDownload) {
+      this.pageDownloads(this.skip);
+    }
+  }
+
+  isSelected(workspaceId: string): boolean {
+    return this.selected.some((w) => w.id === workspaceId);
+  }
+
+  selectAll(event: any): void {
+    if (event.target.checked) {
+      this.source.forEach((w) => {
+        if (!this.selected.some((s) => s.id === w.id)) {
+          this.selected.push(w);
+        }
+      });
+      this.selected.sort((a, b) =>
+        a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1
+      );
+    } else {
+      this.selected.length = 0;
+    }
+  }
+
+  areAllSelected(): boolean {
+    return this.selected.length === this.source.length;
+  }
+
+  setReadyToDownload() {
+    this.readyToDownload = true;
+    this.pageDownloads(0);
+  }
+
+  pageDownloads(s: number): void {
+    console.log('pageDownloads ' + s + '/' + this.selected.length);
+    if (s === this.selected.length) {
+      s = s - this.take;
+      console.log('adjusted to ' + s + '/' + this.selected.length);
+    }
+    this.skip = s;
+    const endIndex =
+      this.selected.length > this.skip + this.take
+        ? this.skip + this.take
+        : this.selected.length;
+    console.log('start/end ' + s + '/' + endIndex);
+    const workspaces = this.selected.slice(s, endIndex);
+    this.selected$.next(workspaces);
+    this.count = endIndex - this.skip;
+    this.search.skip = this.skip;
+    this.search.take = this.take;
+  }
+
+  download(): void {
+    this.isDownloading = true;
+    const workspaceIds =
+      this.selected.length == 0 ? ['all'] : this.selected.map((w) => w.id);
+    this.api.downloadWorkspaces(workspaceIds).subscribe(
+      (data) => {
+        const blob = new Blob([data], {
+          type: 'application/zip',
+        });
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.target = '_blank';
+        link.download = this.getCurrentDateTime() + '-workspaces.zip';
+        this.resetDownload();
+        link.click();
+      },
+      (err) => {
+        window.alert('Error downloading file:  ' + err.message);
+        this.resetDownload();
+      },
+      () => {
+        this.resetDownload();
+      }
+    );
+  }
+
+  resetDownload(): void {
+    this.selected.length = 0;
+    this.isDownloading = false;
+    this.readyToDownload = false;
+    this.selectDownloads = false;
+    this.showDownload = false;
+    this.count = 0;
+    this.skip = 0;
+    this.refresh$.next(true);
+  }
+
+  /**
+   * Selects the file(s) to be uploaded. Called when file selection is changed
+   */
+  upload(e: any) {
+    if (!e.target.files) {
+      // this.areButtonsDisabled = false;
+      return;
+    }
+    // this.uploadProgress = 0;
+    this.api.uploadWorkspaces(e.target.files).subscribe(
+      (result) => {
+        alert(
+          'The following ' +
+            result.length +
+            ' workspaces were uploaded ' +
+            result.join(', ')
+        );
+        this.showUpload = false;
+      },
+      (err) => {
+        window.alert('Error uploading file:  ' + err.message);
+      },
+      () => {}
+    );
+    this.jsonInput.nativeElement.value = '';
+    this.zipInput.nativeElement.value = '';
+  }
+
+  getCurrentDateTime(): string {
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();
+    const month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    const day = currentDate.getDate().toString().padStart(2, '0');
+    const hours = currentDate.getHours().toString().padStart(2, '0');
+    const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+    const seconds = currentDate.getSeconds().toString().padStart(2, '0');
+    return `${year}${month}${day}${hours}${minutes}${seconds}`;
+  }
+
+  getDownloadButtonClass() {
+    const buttonClass =
+      this.selected.length == 0
+        ? 'btn btn-outline-info btn-sm mx-1'
+        : 'btn btn-outline-info btn-sm mx-1 text-warn';
+    return buttonClass;
   }
 }
--- projects/topomojo-work/src/app/api/gen/workspace.service.ts
+++ projects/topomojo-work/src/app/api/gen/workspace.service.ts
@@ -2,12 +2,26 @@
 // Released under a 3 Clause BSD-style license. See LICENSE.md in the project root.
 
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { iif, Observable, of } from 'rxjs';
 import { ApiSettings } from '../api-settings';
 import { GeneratedService } from './_service';
 // tslint:disable-next-line:max-line-length
-import { ChangedWorkspace, NewWorkspace, Search, Workspace, JoinCode, WorkspaceSummary, VmOptions, ChallengeSpec, IsoFile, IsoDataFilter, WorkspaceStats, TemplateSummary, Worker } from './models';
+import {
+  ChangedWorkspace,
+  NewWorkspace,
+  Search,
+  Workspace,
+  JoinCode,
+  WorkspaceSummary,
+  VmOptions,
+  ChallengeSpec,
+  IsoFile,
+  IsoDataFilter,
+  WorkspaceStats,
+  TemplateSummary,
+  Worker,
+} from './models';
 import { map, tap } from 'rxjs/operators';
 
 @Injectable()
@@ -15,74 +29,149 @@ export class GeneratedWorkspaceService extends GeneratedService {
   isoCache!: IsoFile[];
   isoCacheId = '';
 
-  constructor(
-      protected http: HttpClient,
-      protected conf: ApiSettings
-  ) { super(http, conf); }
+  constructor(protected http: HttpClient, protected conf: ApiSettings) {
+    super(http, conf);
+  }
 
   public list(search: Search): Observable<WorkspaceSummary[]> {
-      return this.http.get<WorkspaceSummary[]>(this.conf.api + '/workspaces' + this.paramify(search));
+    return this.http.get<WorkspaceSummary[]>(
+      this.conf.api + '/workspaces' + this.paramify(search)
+    );
   }
   public load(id: string): Observable<Workspace> {
-      return this.http.get<Workspace>(this.conf.api + '/workspace/' + id).pipe(
-        tap(w => w.templates = w.templates?.sort((a, b) => a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1))
+    return this.http
+      .get<Workspace>(this.conf.api + '/workspace/' + id)
+      .pipe(
+        tap(
+          (w) =>
+            (w.templates = w.templates?.sort((a, b) =>
+              a.name.toLowerCase() < b.name.toLowerCase() ? -1 : 1
+            ))
+        )
       );
   }
   public create(model: NewWorkspace): Observable<Workspace> {
-      return this.http.post<Workspace>(this.conf.api + '/workspace', model);
+    return this.http.post<Workspace>(this.conf.api + '/workspace', model);
   }
   public clone(id: string): Observable<Workspace> {
-      return this.http.post<Workspace>(this.conf.api + '/workspace/' + id + '/clone', {});
+    return this.http.post<Workspace>(
+      this.conf.api + '/workspace/' + id + '/clone',
+      {}
+    );
   }
   public update(model: ChangedWorkspace): Observable<any> {
-      return this.http.put<any>(this.conf.api + '/workspace', model);
+    return this.http.put<any>(this.conf.api + '/workspace', model);
   }
   public privilegedUpdate(model: ChangedWorkspace): Observable<any> {
-      return this.http.put<any>(this.conf.api + '/workspace/priv', model);
+    return this.http.put<any>(this.conf.api + '/workspace/priv', model);
   }
   public delete(id: string): Observable<any> {
-      return this.http.delete<any>(this.conf.api + '/workspace/' + id);
+    return this.http.delete<any>(this.conf.api + '/workspace/' + id);
   }
   public getStats(id: string): Observable<WorkspaceStats> {
-      return this.http.get<WorkspaceStats>(this.conf.api + '/workspace/' + id + '/stats');
+    return this.http.get<WorkspaceStats>(
+      this.conf.api + '/workspace/' + id + '/stats'
+    );
   }
   public deleteWorkspaceGames(id: string): Observable<WorkspaceStats> {
-      return this.http.delete<WorkspaceStats>(this.conf.api + '/workspace/' + id + '/games');
+    return this.http.delete<WorkspaceStats>(
+      this.conf.api + '/workspace/' + id + '/games'
+    );
   }
   public generateInvitation(id: string): Observable<JoinCode> {
-      return this.http.put<JoinCode>(this.conf.api + '/workspace/' + id + '/invite', {});
+    return this.http.put<JoinCode>(
+      this.conf.api + '/workspace/' + id + '/invite',
+      {}
+    );
   }
   public enlist(code: string): Observable<WorkspaceSummary> {
-      return this.http.post<WorkspaceSummary>(this.conf.api + '/worker/' + code, {});
+    return this.http.post<WorkspaceSummary>(
+      this.conf.api + '/worker/' + code,
+      {}
+    );
   }
   public deleteWorker(worker: Worker): Observable<any> {
-      return this.http.delete<any>(this.conf.api + `/workspace/${worker.workspaceId}/worker/${worker.subjectId}`);
+    return this.http.delete<any>(
+      this.conf.api +
+        `/workspace/${worker.workspaceId}/worker/${worker.subjectId}`
+    );
   }
   public getWorkspaceIsos(id: string): Observable<VmOptions> {
-      return this.http.get<VmOptions>(this.conf.api + '/workspace/' + id + '/isos');
+    return this.http.get<VmOptions>(
+      this.conf.api + '/workspace/' + id + '/isos'
+    );
   }
   public getWorkspaceNets(id: string): Observable<VmOptions> {
-      return this.http.get<VmOptions>(this.conf.api + '/workspace/' + id + '/nets');
+    return this.http.get<VmOptions>(
+      this.conf.api + '/workspace/' + id + '/nets'
+    );
   }
   public getWorkspaceTemplates(id: string): Observable<TemplateSummary[]> {
-    return this.http.get<TemplateSummary[]>(this.conf.api + '/workspace/' + id + '/templates');
+    return this.http.get<TemplateSummary[]>(
+      this.conf.api + '/workspace/' + id + '/templates'
+    );
   }
   public getWorkspaceChallenge(id: string): Observable<ChallengeSpec> {
     return this.http.get<ChallengeSpec>(this.conf.api + '/challenge/' + id);
   }
-  public putWorkspaceChallenge(id: string, model: ChallengeSpec): Observable<any> {
-      return this.http.put<any>(this.conf.api + '/challenge/' + id, model);
+  public putWorkspaceChallenge(
+    id: string,
+    model: ChallengeSpec
+  ): Observable<any> {
+    return this.http.put<any>(this.conf.api + '/challenge/' + id, model);
   }
   public getIsos(id: string, filter: IsoDataFilter): Observable<IsoFile[]> {
-    return iif(() => filter.refresh || !this.isoCache || this.isoCacheId !== id,
-      this.http.get<VmOptions>(this.conf.api + '/workspace/' + id + '/isos').pipe(
-        map(r => (r.iso || []).map(path => ({path, display: path.split('/').pop()}) as IsoFile)),
-        tap(r => { this.isoCache = r; this.isoCacheId = id; })
-      ),
+    return iif(
+      () => filter.refresh || !this.isoCache || this.isoCacheId !== id,
+      this.http
+        .get<VmOptions>(this.conf.api + '/workspace/' + id + '/isos')
+        .pipe(
+          map((r) =>
+            (r.iso || []).map(
+              (path) => ({ path, display: path.split('/').pop() } as IsoFile)
+            )
+          ),
+          tap((r) => {
+            this.isoCache = r;
+            this.isoCacheId = id;
+          })
+        ),
       of(this.isoCache)
     ).pipe(
-      map(list => filter.local ? list.filter(i => i.path.match(id)) : list),
-      map(list => filter.term ? list.filter(i => i.display.match(filter.term || '')) : list)
+      map((list) =>
+        filter.local ? list.filter((i) => i.path.match(id)) : list
+      ),
+      map((list) =>
+        filter.term
+          ? list.filter((i) => i.display.match(filter.term || ''))
+          : list
+      )
     );
+  }
+  public exportWorkspaces(ids: string[]): Observable<any> {
+    return this.http.post<any>(this.conf.api + '/admin/export', ids);
+  }
+  public importWorkspaces(): Observable<string[]> {
+    return this.http.get<any>(this.conf.api + '/admin/import');
+  }
+  public downloadWorkspaces(ids: string[]): Observable<any> {
+    const headers = { responseType: 'blob' as 'json' };
+    return this.http.post<Blob>(
+      this.conf.api + '/admin/download',
+      ids,
+      headers
+    );
+  }
+  public uploadWorkspaces(files: File[]): Observable<string[]> {
+    const formData = new FormData();
+    for (let index = 0; index < files.length; index++) {
+      const file = files[index];
+      const filename = file.webkitRelativePath
+        ? file.webkitRelativePath
+        : file.name;
+      formData.append('files', file, filename);
+    }
+
+    return this.http.post<string[]>(this.conf.api + '/admin/upload', formData);
   }
 }
