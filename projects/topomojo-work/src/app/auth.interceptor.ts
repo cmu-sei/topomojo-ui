7a1a59ba1c976ef71c3766b9f3c8b31efd24a8c4
Adam Welle <arwelle@sei.cmu.edu>
2026-08-19 18:45:00 +0000

    refactor: use direct API access in development

--- projects/topomojo-work/src/app/auth.interceptor.ts
+++ projects/topomojo-work/src/app/auth.interceptor.ts
@@ -19,13 +19,16 @@ export class AuthInterceptor implements HttpInterceptor {
 
         const authorization = this.injector.get(AuthService).auth_header();
         if (authorization === 'no_token') {
-            return next.handle(req);
+            return next.handle(req.clone({ withCredentials: true }));
         }
 
         return next.handle(
-            req.clone({setHeaders: {
-                Authorization: authorization
-            }})
+            req.clone({
+                setHeaders: {
+                    Authorization: authorization
+                },
+                withCredentials: true
+            })
         );
     }
 }
